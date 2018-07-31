d7f6cf77f7ca7eb505bba22c69fd35c1de5c3c5a
tww <710752055@qq.com>
2018-07-31 14:57:03 +0800

    update

--- app/page/movie/MoviePage.js
+++ app/page/movie/MoviePage.js
@@ -1,6 +1,13 @@
 import React, {PureComponent} from 'react';
-import {Text, View} from "react-native";
+import {Text, View, StyleSheet} from "react-native";
 import SplashScreen from "react-native-splash-screen";
+import {queryThemeColor} from "../../data/realm/RealmManager";
+import HttpMovieManager from "../../data/http/HttpMovieManager";
+import {show} from "../../utils/ToastUtils";
+import ErrorBean from "../../data/http/ErrorBean";
+
+const itemHight = 200;
+const moviesCount = 20;
 
 export default class MoviePage extends PureComponent {
 
@@ -10,14 +17,21 @@ export default class MoviePage extends PureComponent {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            hotMovies: {},
+            refreshing: true,
+            isInit: false,
+            MainColor: queryThemeColor(),
+        };
+        this.httpMovies = new HttpMovieManager();
+        this.requestData();
     }
 
 
     componentDidMount() {
         //还是有白屏看来方法后只能这样，后期有时间再改进
         this.timer = setTimeout(() => {
-            SplashScreen.hide()
+            // SplashScreen.hide();
         }, 100)
     }
 
@@ -35,4 +49,51 @@ export default class MoviePage extends PureComponent {
         )
     }
 
+    requestData() {
+        let start = 0;
+        if (this.state.hotMovies.start != null) {
+            start = this.state.hotMovies.state + 2;
+            if (this.state.hotMovies.total <= this.state.hotMovies.start) {
+                this.setState({
+                    refreshing: false,
+                });
+                show("已是最新数据");
+                return;
+            }
+        }
+
+        this.httpMovies.getHottingMovie(this.state.isInit, start, moviesCount)
+            .then((movies) => {
+                console.log("movies = " + movies);
+                let preSubjects = this.state.hotMovies.subjects;
+                if (preSubjects != null && subjects.length > 0) {
+                    preSubjects.filter((item, i) => {
+                        return i < moviesCount;
+                    }).forEach((item, i) => {
+                        movies.subjects.push(item);
+                    })
+                }
+                this.setState({
+                    hotMovies: movies,
+                    refreshing: false,
+                    isInit: true,
+                });
+
+            }).catch((error) => {
+            if (error != null && error instanceof ErrorBean) {
+                show(error.getErrorMsg());
+            } else {
+                show("net error");
+            }
+
+            this.setState({
+                refreshing: false
+            })
+        });
+
+    }
+
+
 }
+
+const styles = StyleSheet.create({});
